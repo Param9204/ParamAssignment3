701e88006cafa93020a19301428df3201f50f99f
Param9204 <109071960+Param9204@users.noreply.github.com>
2022-09-15 23:17:16 +0530

    Add files via upload

---
+++ Prec3_6.java
@@ -0,0 +1,12 @@
+import Mypack.Myclass1;
+import Mypack.MyClass2;
+public class Prec3_6
+{
+    public static void main(String[] args)
+{
+    Myclass1 m1 = new Myclass1(80);
+    MyClass2 m2 = new MyClass2(100);
+    System.out.println(m1);
+    System.out.println(m2);
+}
+}
